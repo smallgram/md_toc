3d8007c59adfb682a939385fd1b189937edaf349
smallgram <30378053+smallgram@users.noreply.github.com>
2021-06-15 01:14:40 -0400

    Refactor main CLI argument handling
    Also now allows for folders to be entered for the input paths.
    Now also takes the output path to be a folder and uses the default
    blank value to mean to replace the input files in place. Closes #4.

--- src/md_toc.nim
+++ src/md_toc.nim
@@ -12,6 +12,12 @@ type
     level: int
 
 
+proc getMarkdownFiles(dir: string): seq[string] =
+  for kind, path in walkDir(dir):
+    if kind == pcFile and path.splitFile.ext == ".md":
+      result.add path
+
+
 proc extractHeaders(markdown: string): seq[Header] =
   ## Extract the header heirarchy from a passed in chunk of `markdown`
   let headerPattern = re"^ *(#{1,6}) *(.*?) *#* *$"
@@ -39,51 +45,88 @@ proc createTOC(outputPath: string = "",
                top: bool = false,
                inputPaths: seq[string]): bool =
   ## Create a TOC for an input markdown file. This is the program entry.
-  for inputPath in inputPaths:
-    var outPath: string = 
-      if outputPath == "":
-        inputPath 
+
+  # all the file paths to process
+  var fileBatch: seq[string] = @[]
+  var outPath: string = ""
+
+  # these path can be files or directories
+  for path in inputPaths:
+    if fileExists(path):
+      if path.splitFile.ext == ".md":
+        echo "it's a markdown file"
+        fileBatch.add path
       else:
-        outputPath
-    var (outDir, _, _) = outPath.splitFile()
-    if outDir == "":
-      outDir = "."
-
-    if not fileExists(inputPath):
-      raise newException(IOError, "That input file could not be found.") 
-    if not existsOrCreateDir(outDir):
-      echo absolutePath(outDir) & " - OUTPUT FOLDER CREATED."
+        echo "not a markdown file: " & path
     else:
-      echo "OUTPUT FOLDER EXISTS."
+      echo "not a file"
+      if dirExists path:
+        let files = path.getMarkdownFiles()
+        fileBatch.add files
+        echo "it's a directory with " & $(files.len) & " markdown files"
+      else:
+        echo "ERROR: not a markdown file or directory: " & path
+       
+  if fileBatch.len <= 0:
+    echo "no markdown files were found..."
+    return false
+  else:
+    echo "found these files: "
+    for i, f in fileBatch: echo $i & ") " & f
 
-    # Extract the headers from the inputPath file
-    var text = inputPath.readFile()
+  
+  # create outputPath if needed
+
+  if outputPath != "":
+    if dirExists(outputPath):
+      outPath = outputPath.normalizedPath.absolutePath
+    else:
+      if outputPath.splitFile.ext == "":
+        outPath = outputPath.normalizedPath.absolutePath
+      else:
+        outPath = outputPath.splitFile.dir.normalizedPath.absolutePath
+
+      if existsOrCreateDir(outPath):
+        echo "using output path: " & outPath
+      else:
+        echo "created output path: " & outPath
+
+
+  for file in fileBatch:
+    # process each file in the batch 
+    echo "processing file: " & file
+    var text = file.readFile()
     var headers = extractHeaders(text)
 
-    # Generate the TOC text from the header data
+    # Generate the TOC text from the processed header data
     var toc = generateTOC(headers)
 
+    # if --inject=false, then just output the TOC
     if not inject:
+      echo "TOC:"
       echo toc
       continue
 
-    # Open the output file for injection of the TOC text
-    echo "WRITING NEW FILE"
+    # if outputPath is set to something, then create a new file in outputPath
+    #   with the same filename as the input file
+    var outFile = file
+    if outPath != "":
+      outFile = outPath / outFile.splitFile.name & outFile.splitFile.ext
+
+    # try to replace the text and write the new file
+    echo "writing file: " & outFile
     try:
       if top:
         text.insert(toc, 0)
       else:
         text = text.replace("<<TOC>>", toc)
-
-      outPath.writeFile(text)
-
+      outFile.writeFile(text)
       continue 
 
     except:
       raise newException(IOError, "Could not write to output file.")
-
+  
   return true
-
 
 
 when isMainModule:
